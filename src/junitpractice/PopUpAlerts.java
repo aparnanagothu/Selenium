82b4f0c6e83fc75ee7afd04e2a04037540f69642
aparnanagothu <aparnanagothu13@gmail.com>
2025-03-28 07:50:29 +0530

    Import options added

--- src/junitpractice/PopUpAlerts.java
+++ src/junitpractice/PopUpAlerts.java
@@ -1,5 +1,8 @@
 package junitpractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class PopUpAlerts {
