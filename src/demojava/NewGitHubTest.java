8b03270bb147a228df10d693fbc983954ffcfd4b
aparnanagothu <aparnanagothu13@gmail.com>
2025-03-28 08:08:19 +0530

    Prashanth Update NewGitHubTest.java

--- src/demojava/NewGitHubTest.java
+++ src/demojava/NewGitHubTest.java
@@ -2,13 +2,19 @@ package demojava;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewGitHubTest {
+ WebDriver driver;
   @Test
-  public void f() {
-  }
-  @BeforeTest
-  public void beforeTest() {
+  public void techlearn() {
+     driver.get("https://www.Seleniumlearn.com/");   
   }
 
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+  }
 }
