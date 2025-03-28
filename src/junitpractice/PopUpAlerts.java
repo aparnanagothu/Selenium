c3ed57e06d0521235021a8cabdcd3e231d14d857
aparnanagothu <aparnanagothu13@gmail.com>
2025-03-28 07:47:49 +0530

    Update PopUpAlerts by Zakeer.java

--- src/junitpractice/PopUpAlerts.java
+++ src/junitpractice/PopUpAlerts.java
@@ -3,7 +3,16 @@ package junitpractice;
 import org.testng.annotations.Test;
 
 public class PopUpAlerts {
+  WebDriver driver;
   @Test
-  public void f() {
+  public void techlearn() {
+     driver.get("https://www.techlearn.in/");
+    driver.switchTo().alert().accept();
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 }
